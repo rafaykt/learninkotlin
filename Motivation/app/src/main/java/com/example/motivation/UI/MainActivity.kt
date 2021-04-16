e5d89b009d93ddfe90402e053f803cd93ad0ee40
Rafael <rafael.ito92@hotmail.com>
2021-04-16 16:15:04 -0300

    logica das frases funcionando, junto com os botões

--- Motivation/app/src/main/java/com/example/motivation/UI/MainActivity.kt
+++ Motivation/app/src/main/java/com/example/motivation/UI/MainActivity.kt
@@ -7,12 +7,14 @@ import android.view.View
 import com.example.motivation.Infra.MotivationConstants
 import com.example.motivation.Infra.SecurityPreferences
 import com.example.motivation.R
+import com.example.motivation.repository.Mock
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mSecurityPreferences: SecurityPreferences
+    private var mPhraseFilter: Int = MotivationConstants.PHRASEFILTER.ALL
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,12 +22,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
+        //Lógica inicial de seleção
+        imageAll.setColorFilter((resources.getColor(R.color.colorAccent)))
+        handleNewPhrase()
+
         mSecurityPreferences = SecurityPreferences(this);
         textName.text = mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
         buttonNewPhrase.setOnClickListener(this)
         imageMorning.setOnClickListener(this)
         imageAll.setOnClickListener(this)
         imageHappy.setOnClickListener(this)
+
     }
 
     override fun onClick(view: View) {
@@ -46,17 +53,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when(id){
             R.id.imageAll -> {
                 imageAll.setColorFilter((resources.getColor(R.color.colorAccent)))
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.ALL
             }
             R.id.imageHappy -> {
                 imageHappy.setColorFilter((resources.getColor(R.color.colorAccent)))
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.HAPPY
             }
+
             R.id.imageMorning -> {
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.MORNING
                 imageMorning.setColorFilter((resources.getColor(R.color.colorAccent)))
             }
         }
     }
 
     private fun handleNewPhrase(){
-
+        frase.text = Mock().getPhrase(mPhraseFilter)
     }
 }
