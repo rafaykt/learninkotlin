a675cb71cbc47124bb46718417acfcfd6d66c9b9
Rafael <rafael.ito92@hotmail.com>
2021-05-11 16:09:33 -0300

    adicionando viewbinding configs para adapters e viewholders

--- Desafio_filme20/app/src/main/java/com/example/desafio_filme20/view/viewholder/MovieViewHolder.kt
+++ Desafio_filme20/app/src/main/java/com/example/desafio_filme20/view/viewholder/MovieViewHolder.kt
@@ -7,60 +7,43 @@ import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.desafio_filme20.R
+import com.example.desafio_filme20.databinding.RowListFilmsBinding
 import com.example.desafio_filme20.service.listeners.MovieListener
 import com.example.desafio_filme20.service.model.Film
 import com.squareup.picasso.Picasso
 
-class MovieViewHolder(itemView: View, val listener: MovieListener, var filme: Film? = null) :
-    RecyclerView.ViewHolder(itemView) {
+class MovieViewHolder(private val binding: RowListFilmsBinding, val listener: MovieListener, var filme: Film? = null) :
+    RecyclerView.ViewHolder(binding.root) {
 
-    private var mTextTitulo: TextView = itemView.findViewById(R.id.title_movie)
-    private var mTextOverview: TextView = itemView.findViewById(R.id.overview_movie)
-    private var mPoster: ImageView = itemView.findViewById(R.id.poster_movie)
-    private var mBody: ConstraintLayout = itemView.findViewById(R.id.body)
     private val baseUrlFilme = "https://image.tmdb.org/t/p/w200"
 
-    private val mFavoriteStar: ImageButton = itemView.findViewById(R.id.btn_favorite_film)
     /**
      * Atribui valores aos elementos de interface e também eventos
      */
     fun bindData(film: Film) {
 
-        this.mTextTitulo.text = film.original_title
-        this.mTextOverview.text = film.overview
-        Picasso.get().load(baseUrlFilme+film.poster_path).into(mPoster)
+        binding.titleMovie.text = film.original_title
+        binding.overviewMovie.text = film.overview
+        Picasso.get().load(baseUrlFilme+film.poster_path).into(binding.posterMovie)
         if(film.favorite){
-
-            mFavoriteStar.setImageResource(R.drawable.ic_favorite)
+            binding.btnFavoriteFilm.setImageResource(R.drawable.ic_favorite)
         }else{
-            mFavoriteStar.setImageResource(R.drawable.ic_not_favorite)
-
+            binding.btnFavoriteFilm.setImageResource(R.drawable.ic_not_favorite)
         }
 
         // Eventos
-        mBody.setOnClickListener { listener.onListClick(film) }
-        mFavoriteStar.setOnClickListener {
+
+        binding.body.setOnClickListener { listener.onListClick(film) }
+        binding.btnFavoriteFilm.setOnClickListener {
             if(film.favorite){
                 listener.undoFavorite(film)
-                mFavoriteStar.setImageResource(R.drawable.ic_not_favorite)
+                binding.btnFavoriteFilm.setImageResource(R.drawable.ic_not_favorite)
             }else{
                 listener.onFavorite(film)
-                mFavoriteStar.setImageResource(R.drawable.ic_favorite)
+                binding.btnFavoriteFilm.setImageResource(R.drawable.ic_favorite)
             }
         }
 
-//
-//        mTextDescription.setOnLongClickListener {
-//            AlertDialog.Builder(itemView.context)
-//                .setTitle(R.string.remocao_de_tarefa)
-//                .setMessage(R.string.remover_tarefa)
-//                .setPositiveButton(R.string.sim) { dialog, which ->
-//                    listener.onDeleteClick(task.id)
-//                }
-//                .setNeutralButton(R.string.cancelar, null)
-//                .show()
-//            true
-//        }
 
     }
 
