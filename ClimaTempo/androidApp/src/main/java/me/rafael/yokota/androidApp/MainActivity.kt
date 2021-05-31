733b482a28a3f34ad4a97497134728964d151603
Rafael <rafael.ito92@hotmail.com>
2021-05-31 09:52:38 -0300

    parte estável, printa o result na main screen

--- ClimaTempo/androidApp/src/main/java/me/rafael/yokota/androidApp/MainActivity.kt
+++ ClimaTempo/androidApp/src/main/java/me/rafael/yokota/androidApp/MainActivity.kt
@@ -8,25 +8,24 @@ import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
 
 
-
 class MainActivity : AppCompatActivity() {
-
+    private val job = Job()
+    private val scopeIo = CoroutineScope(job + Dispatchers.IO)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val tv: TextView = findViewById(R.id.text_view)
+        lifecycleScope.launch{
+            println(Thread.currentThread().name)
+            tv.text = Greeting().greeting(-21.2115F,-50.4261F)
+        }
 
-
-        greet(tv)
 
     }
 
-    fun greet(tview: TextView) {
-        var resp = ""
-            lifecycleScope.launch(Dispatchers.IO){
-                 tview.text = Greeting().greeting(-21.2115F,-50.4261F)
-            }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
-
 }
