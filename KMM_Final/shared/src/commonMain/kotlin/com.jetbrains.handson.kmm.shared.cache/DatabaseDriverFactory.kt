71a23a103682d9a65a1538118fd609b1af55240e
Rafael <rafael.ito92@hotmail.com>
2021-06-11 11:59:41 -0300

    Initial commit Aplicativo de corrida

--- KMM_Final/shared/src/commonMain/kotlin/com.jetbrains.handson.kmm.shared.cache/DatabaseDriverFactory.kt
+++
@@ -1,7 +0,0 @@
-package me.rafael.yokota.shared.com.jetbrains.handson.kmm.shared.cache
-
-import com.squareup.sqldelight.db.SqlDriver
-
-expect class DatabaseDriverFactory {
-    fun createDriver(): SqlDriver
-}
--- KMM_Final/shared/src/commonMain/kotlin/me/rafael/yokota/shared/Platform.kt
+++
@@ -1,5 +0,0 @@
-package me.rafael.yokota.shared
-
-expect class Platform() {
-    val platform: String
-}
