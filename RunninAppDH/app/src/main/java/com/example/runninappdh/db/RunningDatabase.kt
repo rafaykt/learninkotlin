adac302bcb8129e617bc2c119aa6edb4349cecdc
Rafael <rafael.ito92@hotmail.com>
2021-06-14 14:31:54 -0300

    Configurações no gradle e do Dagger hilt

--- RunninAppDH/app/src/main/java/com/example/runninappdh/db/RunningDatabase.kt
+++ RunninAppDH/app/src/main/java/com/example/runninappdh/db/RunningDatabase.kt
@@ -2,7 +2,6 @@ package com.example.runninappdh.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import com.example.runninappdh.db.dao.RunDAO
 
@@ -10,6 +9,7 @@ import com.example.runninappdh.db.dao.RunDAO
 @Database(
     entities =[Run::class],
     version = 1,
+    exportSchema = false
 )
 
 @TypeConverters(Converters::class)
