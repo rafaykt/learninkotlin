20b52813c3b66bfc5f94d1b23fd0401c2115cd11
Rafael <rafael.ito92@hotmail.com>
2021-06-14 18:15:24 -0300

    Configurando o Easy Permissions

--- RunninAppDH/app/src/main/java/com/example/runninappdh/other/Constants.kt
+++ RunninAppDH/app/src/main/java/com/example/runninappdh/other/Constants.kt
@@ -2,4 +2,5 @@ package com.example.runninappdh.other
 
 object Constants {
     const val RUNNING_DATABASE_NAME = "running_db"
+    const val REQUEST_CODE_PERMISSION = 0
 }
--- RunninAppDH/app/src/main/java/com/example/runninappdh/ui/MainActivity.kt
+++ RunninAppDH/app/src/main/java/com/example/runninappdh/ui/MainActivity.kt
@@ -3,22 +3,33 @@ package com.example.runninappdh.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.runninappdh.R
 import com.example.runninappdh.db.dao.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var runDao: RunDAO
-
-
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("rundao", "Rundao: ${runDao.hashCode()}")
+
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener{ _, destination, _ ->
+                when(destination.id){
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility= View.VISIBLE
+                    else ->
+                        bottomNavigationView.visibility=View.GONE
+                }
+            }
     }
 }
