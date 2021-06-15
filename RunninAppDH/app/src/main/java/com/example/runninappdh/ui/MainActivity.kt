26030308e3a04194eed9c4751077c161549ac914
Rafael <rafael.ito92@hotmail.com>
2021-06-15 14:37:39 -0300

    Tracking funcionando em foreground

--- RunninAppDH/app/src/main/java/com/example/runninappdh/ui/MainActivity.kt
+++ RunninAppDH/app/src/main/java/com/example/runninappdh/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.runninappdh.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.runninappdh.R
 import com.example.runninappdh.db.dao.RunDAO
+import com.example.runninappdh.other.Constants
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
@@ -18,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigateToTrackingFragmentIfNeeded(intent)
 
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
@@ -31,5 +35,19 @@ class MainActivity : AppCompatActivity() {
                         bottomNavigationView.visibility=View.GONE
                 }
             }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (intent != null) {
+            navigateToTrackingFragmentIfNeeded(intent)
+        }
+
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent){
+        if(intent?.action== Constants.ACTION_SHOW_TRACKING_FRAGMENT){
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
     }
 }
