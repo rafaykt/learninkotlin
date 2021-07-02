6af169fec049a32fcb60f9abdd2dccc9a49ae423
Rafael <rafael.ito92@hotmail.com>
2021-07-02 17:06:36 -0300

    Filtros para listar

--- RunninAppDH/app/src/main/java/com/example/runninappdh/ui/fragments/RunFragment.kt
+++ RunninAppDH/app/src/main/java/com/example/runninappdh/ui/fragments/RunFragment.kt
@@ -5,6 +5,7 @@ import android.Manifest
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.runninappdh.R
 import com.example.runninappdh.adapter.RunAdapter
 import com.example.runninappdh.other.Constants.REQUEST_CODE_PERMISSION
+import com.example.runninappdh.other.SortType
 import com.example.runninappdh.other.TrackingUtility
 import com.example.runninappdh.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -33,11 +35,39 @@ class RunFragment: Fragment(R.layout.fragment_run), EasyPermissions.PermissionCa
         super.onViewCreated(view, savedInstanceState)
         requestPermissions()
         setupRecyclerView()
-        viewModel.runsSortedByDate.observe(viewLifecycleOwner, {
+        viewModel.runs.observe(viewLifecycleOwner, {
             runAdapter.submitList(it)
         })
         fab.setOnClickListener{
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+        when(viewModel.sortType){
+            SortType.DATE -> spFilter.setSelection(0)
+            SortType.RUNNING_TIME -> spFilter.setSelection(1)
+            SortType.DISTANCE -> spFilter.setSelection(2)
+            SortType.AVG_SPEED -> spFilter.setSelection(3)
+            SortType.CALORIES_BURNED -> spFilter.setSelection(4)
+        }
+        spFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when(position){
+                    0 -> viewModel.sortRuns(SortType.DATE)
+                    1 -> viewModel.sortRuns(SortType.RUNNING_TIME)
+                    2 -> viewModel.sortRuns(SortType.DISTANCE)
+                    3 -> viewModel.sortRuns(SortType.AVG_SPEED)
+                    4 -> viewModel.sortRuns(SortType.CALORIES_BURNED)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
         }
 
 
