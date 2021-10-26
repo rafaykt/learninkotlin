af74964766ea9dc4701088f872fdc41677d0be98
Rafael <rafael.ito92@hotmail.com>
2021-10-26 12:37:04 -0300

    First notification commit

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -25,6 +25,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
@@ -50,14 +51,24 @@ class EggTimerFragment : Fragment() {
         binding.eggTimerViewModel = viewModel
         binding.lifecycleOwner = this.viewLifecycleOwner
 
-        // TODO: Step 1.7 call create channel
-
+        // TODO: Step 1.7 call create channel createChannel(NotificationChannel.DEFAULT_CHANNEL_ID, getString(R.string.egg_notification_channel_name))
+        createChannel(getString(R.string.egg_notification_channel_id), getString(R.string.egg_notification_channel_name))
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun createChannel(channelId: String, channelName: String) {
         // TODO: Step 1.6 START create a channel
+        val notificationChannel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_LOW)
+        notificationChannel.enableLights(true)
+        notificationChannel.lightColor
+        notificationChannel.enableVibration(true)
+        notificationChannel.description="Time for breakfast"
 
+        val notificationManager = requireActivity().getSystemService(
+            NotificationManager::class.java
+        )
+        notificationManager.createNotificationChannel(notificationChannel)
         // TODO: Step 1.6 END create a channel
 
     }
